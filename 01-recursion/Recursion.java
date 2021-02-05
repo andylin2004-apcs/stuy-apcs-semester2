dc7398720f33acf5885ead9fbddf150dd3d9b993
Andy Lin <alin21@stuy.edu>
2021-02-05 13:15:08 -0500

    reverse added

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -1,11 +1,12 @@
 class Recursion{
   public static String reverse(String s){
-
+    if (s.length() <= 1){ return s; }
+    else { return reverse(s.substring(1, s.length())) + s.charAt(0);}
   }
   public static long countNoDoubleLetterWords(int length,String word){
 
   }
   public static double sqrt(double n){
-    
+
   }
 }
