c956aa02737c24d4acc1c0caa3e33c0ae2d47d62
Andy Lin <alin21@stuy.edu>
2021-02-05 13:19:18 -0500

    added countnodouble

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -4,7 +4,12 @@ class Recursion{
     else { return reverse(s.substring(1, s.length())) + s.charAt(0);}
   }
   public static long countNoDoubleLetterWords(int length,String word){
-
+    if (length == 0){ return 1; }
+    for (int i = 0; i<=26; i++){
+      if (word.charAt(word.length()-1) == 'a'+i){ continue; }
+      else { return countNoDoubleLetterWords(length-1, word+('a'+i)); }
+    }
+    return 0;
   }
   public static double sqrt(double n){
 
