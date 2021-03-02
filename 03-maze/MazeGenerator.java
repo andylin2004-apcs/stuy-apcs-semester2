725efd5ed11b54b522545bafce343ba92790fb32
Andy Lin <alin21@stuy.edu>
2021-03-02 16:58:03 -0500

    should now remove from all four direction when possible

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -16,12 +16,32 @@ class MazeGenerator{
       System.out.println();
     }
   }
-  private static int removeAt(char[][] maze, int row, int col, String from){
-    if (row == 0 || col == 0 || row == maze.length-1 || col == maze[0].length-1){
+  private static int removeAt(char[][] maze, int row, int col, String direction){
+    if (row == 0 || col == 0 || row == maze.length-1 || col == maze[0].length-1 || maze[row][col] == ' '){
       return 0;
     }
     maze[row][col] = ' ';
-    return 0;
+    for (int i = 0; i<maze.length; i++){
+      for (int v = 0; v<maze[0].length; v++){
+        System.out.print(maze[i][v]);
+      }
+      System.out.println();
+    }
+    System.out.println();
+    int returnNum = 0;
+    if (!direction.equals("down")){
+      returnNum += removeAt(maze, row+1, col, "up");
+    }
+    if (!direction.equals("up")){
+      returnNum += removeAt(maze, row-1, col, "down");
+    }
+    if (!direction.equals("right")){
+      returnNum += removeAt(maze, row, col+1, "left");
+    }
+    if (!direction.equals("left")){
+      returnNum += removeAt(maze, row, col-1, "right");
+    }
+    return returnNum;
   }
   public static void main(String[] args) {
     char[][] maze = new char[8][8];
