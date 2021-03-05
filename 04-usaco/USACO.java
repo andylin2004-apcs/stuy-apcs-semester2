4c8067918c3e72bbb5cc342923adf461241d273c
Andy Lin <alin21@stuy.edu>
2021-03-05 17:50:51 -0500

    now able to parse first line through map data info for bronze

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -7,13 +7,16 @@ class USACO{
   public static int bronze(String filename) throws FileNotFoundException{
     Scanner in = new Scanner(new File(filename));
     ArrayList<ArrayList<Integer>> field = new ArrayList<ArrayList<Integer>>();
-    String info = in.nextLine();
-    System.out.println(info);
-    while (in.hasNextLine()){
-      String read = in.nextLine();
+    Scanner initData = new Scanner(in.nextLine());
+    int row = Integer.parseInt(initData.next());
+    int col = Integer.parseInt(initData.next());
+    int goalElev = Integer.parseInt(initData.next());
+    int numInstruct = Integer.parseInt(initData.next());
+    for(int i = 0; i<row; i++){
+      Scanner read = new Scanner(in.nextLine());
       field.add(new ArrayList<Integer>());
-      for (int i = 0; i<read.length(); i+=2){
-        field.get(field.size()-1).add((int)read.charAt(i));
+      while(read.hasNext()){
+        field.get(i).add(Integer.parseInt(read.next()));
       }
     }
     return 0;
