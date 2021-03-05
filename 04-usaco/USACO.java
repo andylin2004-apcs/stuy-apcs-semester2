7426757d0397207084a0f899450f7588a6778895
Andy Lin <alin21@stuy.edu>
2021-03-05 13:26:43 -0500

    parsing field data for bronze from input, intial scanner support for silver

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 class USACO{
-  public static int bronze(String filename){}
-  public static long silver(String filename){}
+  public static int bronze(String filename) throws FileNotFoundException{
+    Scanner in = new Scanner(new File(filename));
+    ArrayList<ArrayList<Integer>> field = new ArrayList<ArrayList<Integer>>();
+    while (in.hasNextLine()){
+      String read = in.nextLine();
+      field.add(new ArrayList<Integer>());
+      for (int i = 0; i<read.length(); i+=2){
+        field.get(field.size()-1).add((int)read.charAt(i));
+      }
+    }
+    return 0;
+  }
+  public static long silver(String filename) throws FileNotFoundException{
+    Scanner in = new Scanner(new File(filename));
+    return 0;
+  }
 }
