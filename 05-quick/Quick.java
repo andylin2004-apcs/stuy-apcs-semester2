79936d4fb7dfbb97ac5c60cc085f6089f3452f8f
Andy Lin <alin21@stuy.edu>
2021-03-14 19:27:10 -0400

    init commit

---
+++ 05-quick/Quick.java
@@ -0,0 +1,15 @@
+class Quick{
+
+          /*return the value that is the kth smallest value of the array.
+           *@param data must have a length > 0
+           *@param k is 0 to data.length-1 inclusive
+           *@postcondition The array can be modified. (If we did not want to modify the array, we could make a copy before we start the process)
+           */
+           public static int quickselect(int []data, int k){ }
+
+           public static int[] partitionDutch(int[],int lo, int hi){
+               //THIS IS OPTIONAL METHOD, it will make your quicksort faster.
+               //return an int array: {lt,gt}
+             }
+           
+}
