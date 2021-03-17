41340199b4f2efa4ff4593396dc1abb0233f4180
Andy Lin <alin21@stuy.edu>
2021-03-17 14:25:58 -0400

    restructure such that it actually funcs as a recursive func

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,9 +1,13 @@
 class Merge{
   public static void mergesort(int[]data){
+    mergesort(data, 0, data.length);
+  }
+  public static int[] mergesort(int[] data, int low, int high){
     if (data.length > 1){
-      //mergesort left
-      //mergesort right
+      mergesort(data, 0, Math.round(data.length/2));
+      mergesort(data, Math.round(data.length/2), data.length);
       //merge together
     }
+    return data;
   }
 }
