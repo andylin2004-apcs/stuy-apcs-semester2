73321789af97728867a7d9bce87166e7654f55b8
Andy Lin <alin21@stuy.edu>
2021-04-06 17:33:34 -0400

    removed unnecessary code

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -88,9 +88,6 @@ public class MyDeque<E>{
     data = newData;
     start = size+start;
     end = size+end;
-    if (size == 0){
-      end++;
-    }
   }
   private void noSuchExc(){
     if (size == 0){
@@ -101,73 +98,5 @@ public class MyDeque<E>{
     if (input == null){
       throw new NullPointerException();
     }
-  }
-
-  public static boolean equals(MyDeque<Integer>a, ArrayDeque<Integer>b){
-    if(a==null && b==null)
-    return true;
-    if(a==null || b==null)
-    return false;
-    if(a.size()!=b.size())
-    return false;
-    if(a.size()==0 && b.size()==0)
-    return true;
-
-    try{
-      while(b.size()>0){
-        if(!a.removeFirst().equals(b.removeFirst())){
-          return false;
-        }
-      }
-    }catch(NoSuchElementException e){
-      return false;
-    }
-    return b.size()==0;
-  }
-
-  public static int test6(int max){
-    MyDeque<Integer> a = new MyDeque<Integer>();
-    ArrayDeque<Integer>b = new ArrayDeque<Integer>();
-
-    for(int i = 0; i < max; i++){
-      int op = (int)(Math.random()*4);
-      if(op == 0){
-        a.addLast(i);
-        b.addLast(i);
-      }
-      if(op == 1){
-        a.addFirst(i);
-        b.addFirst(i);
-      }
-      if(op == 2){
-        if(b.size()>0){
-          if(! a.getLast().equals(b.getLast())){
-            System.out.println("Fail test6a "+a.getLast()+" vs "+b.getLast());
-            return 0;
-          }
-          a.removeLast();
-          b.removeLast();
-        }
-      }
-      if(op == 3){
-        if(b.size()>0){
-          if(! a.getFirst().equals(b.getFirst())){
-            System.out.println("Fail test6a "+a.getFirst()+" vs "+b.getFirst());
-            return 0;
-          }
-          a.removeFirst();
-          b.removeFirst();
-        }
-      }
-    }
-    if( equals(a,b) ){
-      return 1;
-    }
-    System.out.println("Fail test6 end");
-    return 0;
-  }
-
-  public static void main(String[] args) {
-    System.out.println(test6(10000));
   }
 }
